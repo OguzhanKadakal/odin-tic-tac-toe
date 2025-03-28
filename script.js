9fda80aed093b83d657441cc8b12b3e8dacc0c68
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-03-28 21:10:03 +0300

    Implement gameboard rendering and player creation for Tic Tac Toe

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
+const Gameboard = (() => {
+  let gameboard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
 
-const startButton = document.querySelector(".start-game-btn")
+  const render = () => {
+    let boardHTML = "";
+    gameboard.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        boardHTML += `<div id="${rowIndex}-${colIndex}" class="cell">${cell}</div>`;
+      });
+    });
+    document.querySelector(".gameboard").innerHTML = boardHTML;
+  };
+  return {render};
+})();
+
+const createPlayer = (name, mark) => {
+  return {
+    name,
+    mark,
+  };
+};
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O"),
+        ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    };
+    return  {start};
+})();
+
+const startButton = document.querySelector(".start-game-btn");
 startButton.addEventListener("click", () => {
-    //game.start();
-})
+  Game.start();
+});
